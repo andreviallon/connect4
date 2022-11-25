f6c93d422740a335da2050e372a8f40c760118d2
Andre Viallon <viallon.andre@gmail.com>
2022-11-25 17:24:24 +0100

    test: check win conditions

--- utils/CheckWin.spec.ts
+++ utils/CheckWin.spec.ts
@@ -1,4 +1,9 @@
-import { checkHorizontalWin, checkVerticalWin } from "./CheckWin";
+import {
+  checkAntiDiagonalWin,
+  checkDiagonalWin,
+  checkHorizontalWin,
+  checkVerticalWin,
+} from "./CheckWin";
 import { Player } from "./Player";
 
 describe("CheckWin", () => {
@@ -9,7 +14,7 @@ describe("CheckWin", () => {
       expect(res).toBe(false);
     });
 
-    it("should return true if player one has 4 horizontal disks in a row", () => {
+    it("should return true if player one has 4 disks in a row", () => {
       const board = [
         [
           Player.PLAYER_ONE,
@@ -59,7 +64,7 @@ describe("CheckWin", () => {
       expect(res).toBe(false);
     });
 
-    it("should return true if player one has 4 horizontal disks in a row", () => {
+    it("should return true if player one has 4 disks in a column", () => {
       const board = [
         [
           Player.PLAYER_ONE,
@@ -101,7 +106,7 @@ describe("CheckWin", () => {
       expect(res).toBe(true);
     });
 
-    it("should return false if player one has 4 horizontal disks not in a row", () => {
+    it("should return false if player one has 4 disks not in a column", () => {
       const board = [
         [
           Player.PLAYER_ONE,
@@ -112,14 +117,6 @@ describe("CheckWin", () => {
           undefined,
         ],
         [
-          Player.PLAYER_TWO,
-          undefined,
-          undefined,
-          undefined,
-          undefined,
-          undefined,
-        ],
-        [
           Player.PLAYER_ONE,
           undefined,
           undefined,
@@ -135,6 +132,7 @@ describe("CheckWin", () => {
           undefined,
           undefined,
         ],
+        [undefined, undefined, undefined, undefined, undefined, undefined],
         [
           Player.PLAYER_ONE,
           ,
@@ -148,6 +146,147 @@ describe("CheckWin", () => {
         [undefined, undefined, undefined, undefined, undefined, undefined],
       ];
       const res = checkVerticalWin(board);
+      expect(res).toBe(false);
+    });
+  });
+
+  describe("checkDiagonalWin", () => {
+    it("should return false with an empty board", () => {
+      const board = Array.from(Array(7)).map((_) => Array.from(Array(6)));
+      const res = checkDiagonalWin(board);
+      expect(res).toBe(false);
+    });
+
+    it("should return true if player one has 4 disks in diagonal", () => {
+      const board = [
+        [
+          Player.PLAYER_ONE,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+        ],
+        [
+          undefined,
+          Player.PLAYER_ONE,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+        ],
+        [
+          undefined,
+          undefined,
+          Player.PLAYER_ONE,
+          undefined,
+          undefined,
+          undefined,
+        ],
+        [
+          undefined,
+          undefined,
+          undefined,
+          Player.PLAYER_ONE,
+          undefined,
+          undefined,
+        ],
+        [undefined, undefined, undefined, undefined, undefined, undefined],
+        [undefined, undefined, undefined, undefined, undefined, undefined],
+        [undefined, undefined, undefined, undefined, undefined, undefined],
+      ];
+      const res = checkDiagonalWin(board);
+      expect(res).toBe(true);
+    });
+
+    it("should return false if player one does not have 4 disks in diagonal", () => {
+      const board = [
+        [
+          Player.PLAYER_ONE,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+        ],
+        [
+          undefined,
+          Player.PLAYER_ONE,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+        ],
+        [
+          undefined,
+          undefined,
+          Player.PLAYER_TWO,
+          undefined,
+          undefined,
+          undefined,
+        ],
+        [
+          undefined,
+          undefined,
+          undefined,
+          Player.PLAYER_ONE,
+          undefined,
+          undefined,
+        ],
+        [undefined, undefined, undefined, undefined, undefined, undefined],
+        [undefined, undefined, undefined, undefined, undefined, undefined],
+        [undefined, undefined, undefined, undefined, undefined, undefined],
+      ];
+      const res = checkDiagonalWin(board);
+      expect(res).toBe(false);
+    });
+  });
+
+  describe("checkAntiDiagonalWin", () => {
+    it("should return false with an empty board", () => {
+      const board = Array.from(Array(7)).map((_) => Array.from(Array(6)));
+      const res = checkAntiDiagonalWin(board);
+      expect(res).toBe(false);
+    });
+    it("should return true if player one has 4 disks in an anti diagonal", () => {
+      const board = [
+        [
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          Player.PLAYER_ONE,
+        ],
+        [
+          undefined,
+          undefined,
+          undefined,
+          undefined,
+          Player.PLAYER_ONE,
+          undefined,
+        ],
+        [
+          undefined,
+          undefined,
+          undefined,
+          Player.PLAYER_ONE,
+          undefined,
+          undefined,
+        ],
+        [
+          undefined,
+          undefined,
+          Player.PLAYER_ONE,
+          undefined,
+          undefined,
+          undefined,
+        ],
+        [undefined, undefined, undefined, undefined, undefined, undefined],
+        [undefined, undefined, undefined, undefined, undefined, undefined],
+        [undefined, undefined, undefined, undefined, undefined, undefined],
+      ];
+      const res = checkAntiDiagonalWin(board);
       expect(res).toBe(true);
     });
   });
